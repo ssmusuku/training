336d99e513801ea4e29f358677757f7423a2572a
ssmusuku <saimusuku11@gmail.com>
2023-10-12 00:18:52 -0500

    new changes

--- com.exmaples/FirstProgram.java
+++ com.exmaples/FirstProgram.java
@@ -3,5 +3,7 @@ public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("hiii");
+        new code;
+        sssas;
     }
 }
