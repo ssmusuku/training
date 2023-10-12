bb36d19ea6697f0ac30a9438fe895f2c68b1acb0
ssmusuku <saimusuku11@gmail.com>
2023-10-12 00:13:37 -0500

    changed terminal

--- com.exmaples/FirstProgram.java
+++ com.exmaples/FirstProgram.java
@@ -2,6 +2,6 @@ package com.examples
 public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("hi");
+        System.out.println("hiii");
     }
 }
